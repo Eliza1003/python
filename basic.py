2803fc2a9d6a1b53da2df3a30d619087e98142c9
Eliza1003 <imamdad.gva@gmail.com>
2026-09-03 19:46:48 -0400

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,14 @@
+# printing a greeting message
+print("Hello, world!")
+
+# printing my name 
+print ("My name is Alex.")
+
+# printing a fun fact about myself
+print ("I love playing soccer.")
+
+# performing and printing a simple calculation
+print ("The result of 3+5 is:", 3+5)
+
+# printing my name
+print("My name is Eliza.")
