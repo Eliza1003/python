f0b78255f639c6b5c8dfd8a58763ca91a1343681
Eliza1003 <imamdad.gva@gmail.com>
2026-09-08 19:43:45 -0400

    Add files via upload

---
+++ command.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello", 5)
+
+#End agrument of Print Statment
+print("welcome to", end= "*")
